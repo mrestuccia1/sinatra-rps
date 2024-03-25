fcd355627f3d59763290a53867d0f3fa78da6f98
Matteo Restuccia <102557721+mrestuccia1@users.noreply.github.com>
2024-03-25 19:23:29 +0000

    pushing final changes

--- app.rb
+++ app.rb
@@ -1,8 +1,55 @@
 require 'sinatra'
 
+def handle_battle(player, cpu)
+  if player == cpu
+    return "We Tied!"
+
+  elsif player == "Rock" && cpu == "Paper"
+    return "We Lost!"
+
+  elsif player == "Rock" && cpu == "Scissors"
+    return "We Won!"
+
+  elsif player == "Paper" && cpu == "Scissors"
+    return "We Lost!"
+
+  elsif player == "Paper" && cpu == "Rock"
+    return "We Won!"
+
+  elsif player == "Scissors" && cpu == "Paper"
+    return "We Won!"
+
+  elsif player == "Scissors" && cpu == "Rock"
+    return "We Lost!"
+
+  end
+end
+
 get('/') do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:index)
+end
+
+get('/rock') do
+  @player = 'Rock'
+  @cpu = ['Rock', 'Paper', 'Scissors'].sample
+
+  @outcome = handle_battle(@player, @cpu)
+  erb :response
+end
+
+get('/scissors') do
+  @player = 'Scissors'
+  @cpu = ['Rock', 'Paper', 'Scissors'].sample
+
+  @outcome = handle_battle(@player, @cpu)
+  erb :response
+end
+
+get('/paper') do
+  @player = 'Paper'
+  @cpu = ['Rock', 'Paper', 'Scissors'].sample
+
+  @outcome = handle_battle(@player, @cpu)
+  pp @outcome
+  erb :response
 end
